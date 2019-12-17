05ae6c77e5442b7a7d5015fde66e13c93393886f
Jason Loomis <jason@jrloomis.com>
2019-12-17 11:09:33 -0500

    apollo server running

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 
 const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
 
 const middleware = require('../middleware/global')
+const typeDefs = require('../graphql/schema')
+const { resolvers } = require('../graphql')
 
 const app = express()
 const port = process.env.PORT
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
 middleware(app)
+
+server.applyMiddleware({ app, path: '/graphql' })
 
 app.listen(port, () => console.log(`server listening on port ${port}`))
